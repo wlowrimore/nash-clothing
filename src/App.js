c802c38095609e26e8185c86c1a39dbc3cc555c9
William Lowrimore <wlowrimore@gmail.com>
2023-01-05 12:03:08 -0600

    sets up basic categories structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
 const App = () => {
-  return <div>Hello World</div>;
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          {/* <img /> */}
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
