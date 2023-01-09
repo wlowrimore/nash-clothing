b73ecb27d0a410c85b93fabaf2b3df39062512ea
William Lowrimore <wlowrimore@gmail.com>
2023-01-09 13:30:15 -0600

    adds functionality for adding product item(s) to dropdown card.

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,17 @@
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/cart.context";
+
 import "./product-card.styles.scss";
 
 import Button from "../button/button.component";
 
-const ProductCard = ({ product: { id, name, imageUrl, price } }) => {
+const ProductCard = ({ product }) => {
+  const { imageUrl, name, price } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
@@ -10,7 +19,9 @@ const ProductCard = ({ product: { id, name, imageUrl, price } }) => {
         <span className="name">{name}</span>
         <span className="price">${price}</span>
       </div>
-      <Button buttonType="inverted">Add to Cart</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add to Cart
+      </Button>
     </div>
   );
 };
