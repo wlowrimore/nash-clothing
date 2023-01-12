f0a860692911db1c4f717e34e42108bcbfb280a6
William Lowrimore <wlowrimore@gmail.com>
2023-01-12 10:31:01 -0600

    moves categories array into the directory and uses the UseNavigate hoot for navigating from categories page to individual pages via routes

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,12 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 import {
   BackgroundImage,
   Body,
   DirectoryItemContainer,
 } from "./directory-item.styles";
 
-const DirectoryItem = ({ category: { imageUrl, title } }) => {
+const DirectoryItem = ({ category: { imageUrl, title, route } }) => {
+  const navigate = useNavigate();
+
+  const onNavigatHandler = () => navigate(route);
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigatHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
