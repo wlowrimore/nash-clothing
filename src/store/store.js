c631523e5ff8b49cc1223fee618ea4146f235104
William Lowrimore <wlowrimore@gmail.com>
2023-02-01 15:59:33 -0600

    refactor middleware and sets up logger for development only

--- src/store/store.js
+++ src/store/store.js
@@ -5,23 +5,9 @@ import {
 } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-// import logger from "redux-logger";
+import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
-
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currentState: ", store.getState());
-
-  next(action);
-
-  console.log("next state: ", store.getState());
-};
 
 const persistConfig = {
   key: "root",
@@ -31,7 +17,9 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [loggerMiddleware];
+const middlewares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
