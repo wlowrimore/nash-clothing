f36747437a81f93bdf37ea7a607a2156d42aa3fb
William Lowrimore <wlowrimore@gmail.com>
2023-02-01 08:54:08 -0600

    adds reselect library to categories to have it fire only when updated

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,25 @@ import {
   applyMiddleware,
   legacy_createStore as createStore,
 } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const middlewares = [logger];
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log("type: ", action.type);
+  console.log("payload: ", action.payload);
+  console.log("currentState: ", store.getState());
+
+  next(action);
+
+  console.log("next state: ", store.getState());
+};
+
+const middlewares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
