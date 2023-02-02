1f069899250ec2ee2fb44d6f7444c8f84ad8f204
William Lowrimore <wlowrimore@gmail.com>
2023-02-02 17:22:14 -0600

    creates paymentIntent serverless netlify function

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -13,6 +13,7 @@ import {
 } from "./checkout.styles";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from "../../components/payment-form/payment-form.component";
 
 const Checkout = () => {
   const cartItems = useSelector(selectCartItems);
@@ -41,6 +42,7 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <Total>Total: ${cartTotal}</Total>
+      <PaymentForm />
     </CheckoutContainer>
   );
 };
